1534dc3fa167e2795926cebab4895e12b7d091cd
DESKTOP-HBIBUUM\user <test@test.com>
2024-06-17 18:50:40 +0900

    0617 select,update 학원수업 quiz

--- src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
+++ src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
@@ -1,5 +1,7 @@
 package com.quiz.lesson03.bo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,17 +12,39 @@ import com.quiz.lesson03.mapper.RealEstateMapper;
 public class RealEstateBO {
 
 	@Autowired
-	private RealEstateMapper RealEstateMapper;
+	private RealEstateMapper realEstateMapper;
 	// input : id(int)
 	// output : RealEstate(단건) or null
 	public RealEstate getRealEstateById(int id) {
-		return RealEstateMapper.selectRealEstateById(id);
+		return realEstateMapper.selectRealEstateById(id);
 	}
 	
 	// input : rentPrice(필수)
 	// output : List<RealEstate> 단건이 아니기 때문에 List임
-	public RealEstate getRealEstateListByRentPrice(int rentPrice) { // Integer 아니고 int 인 이유?
-		return RealEstateMapper.selectRealEstateListByRentPrice(rentPrice);
+	public List<RealEstate> getRealEstateListByRentPrice(int rentPrice) { // Integer 아니고 int 인 이유?
+		return realEstateMapper.selectRealEstateListByRentPrice(rentPrice);
+	}
+	
+	// input: area , price
+	// output : List<RealEstate>
+	public List<RealEstate> getRealEstateListByAreaPrice(int area, int price) {
+		return realEstateMapper.selectRealEstateListByAreaPrice(area, price);
+	}
+	// input: RealEstate
+	// output : 성공한 행의 횟수 (int)
+	public int addRealEstate(RealEstate realEstate) {
+		return realEstateMapper.insertRealEstate(realEstate);
+	}
+	
+	// input:
+	// output:
+//	address : 썅떼빌리버 오피스텔 814호
+//	area : 45
+//	type : 월세
+//	price : 100000
+//	rentPrice : 120
+	public int addRealEstateAsField(int realtorId , String address, int area, String type, int price, Integer rentPrice) {
+		return realEstateMapper.insertRealEstateAsField(realtorId, address, area, type, price, rentPrice);
 	}
 }
 
--- src/main/java/com/quiz/lesson03/mapper/RealEstateMapper.java
+++ src/main/java/com/quiz/lesson03/mapper/RealEstateMapper.java
@@ -1,6 +1,9 @@
 package com.quiz.lesson03.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.quiz.lesson03.domain.RealEstate;
 
@@ -13,5 +16,22 @@ public interface RealEstateMapper {
 	
 	// input : rentPrice
 	// output : RealEstate
-	public RealEstate selectRealEstateListByRentPrice(int rentPrice);
+	public List<RealEstate> selectRealEstateListByRentPrice(int rentPrice);
+	
+	
+	// mapper 인터페이스가 xml로 파라미터 전송시 1개만 보낼 수 있다.
+	// @Param 어노테이션을 사용하면 파라미터를 하나의 맵으로 구성해준다.
+	public List<RealEstate> selectRealEstateListByAreaPrice(
+			@Param("area") int area, 
+			@Param("price") int price);
+	
+	public int insertRealEstate(RealEstate realEstate);
+	
+	public int insertRealEstateAsField( // 파라미터가 여러 개이므로 map으로 묶기 위해 @Param 어노테이션 사용
+			@Param("realtorId") int realtorId, 
+			@Param("address") String address, 
+			@Param("area") int area, 
+			@Param("type") String type,
+			@Param("price") int price, 
+			@Param("rentPrice") Integer rentPrice);
 }
