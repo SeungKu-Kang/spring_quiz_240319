616d6bcb7fefaa9c90193756874ebf29ec9aaaa5
DESKTOP-HBIBUUM\user <test@test.com>
2024-06-25 18:51:45 +0900

    0625 학원 수업 기상청 quiz 미완성

--- src/main/java/com/quiz/weather_history/WeatherHistoryController.java
+++ src/main/java/com/quiz/weather_history/WeatherHistoryController.java
@@ -1,5 +1,54 @@
 package com.quiz.weather_history;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.quiz.weather_history.bo.WeatherHistoryBO;
+import com.quiz.weather_history.domain.WeatherHistory;
+
+@RequestMapping("/weather_history")
+@Controller
 public class WeatherHistoryController {
 
+	@Autowired
+	private WeatherHistoryBO weatherHistoryBO;
+	
+	// 날씨 목록 화면 /weather_history/weather-history-view
+	@GetMapping("/weather-history-view")
+	public String weatherListView(Model model) {
+		// select db 
+		List<WeatherHistory> weatherHistoryList = weatherHistoryBO.getWeatherHistory();
+		
+		// model에 담기
+		model.addAttribute("weatherHistoryList", weatherHistoryList);
+		
+		// 화면 이동
+		return "/weather_history/weatherList";
+	}
+	
+	// 날씨 추가 화면 /weather-history/add-weather-view
+	@GetMapping("/add-weather-view")
+	public String addWeatherView() {
+		return "weather_history/addWeather";
+	}
+	
+	// 날씨 추가 기능 /weather-history/add-weather
+	@PostMapping("/add-weather")
+	public String addWeather(
+			@RequestParam("date") LocalDate date,
+			@RequestParam("weather") String weather,
+			@RequestParam("temperatures") double temperatures,
+			@RequestParam("precipitation") double precipitation,
+			@RequestParam("microDust") String microDust,
+			@RequestParam("windSpeed") double windSpeed) {
+		return "";
+	}
 }
