fafa790bbc43c1107774863a306994f8c65b072a
DESKTOP-HBIBUUM\user <test@test.com>
2024-07-01 18:50:34 +0900

    0701 ajax quiz 3번

--- src/main/java/com/quiz/booking/BookingController.java
+++ src/main/java/com/quiz/booking/BookingController.java
@@ -1,12 +1,19 @@
 package com.quiz.booking;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.quiz.booking.bo.BookingBO;
 import com.quiz.booking.domain.Booking;
@@ -20,7 +27,7 @@ public class BookingController {
 	
 	
 	
-	// 예약 목록을 나타내는 화면 -- 테이블로 뿌릴거니까 model에 담아야함
+	// 예약 목록을 나타내는 화면 -- view에서 테이블로 뿌릴거니까 model에 담아야함
 	// http://localhost:8080/booking/booking-list-view
 	@GetMapping("/booking-list-view")
 	public String bookingListView(Model model) {
@@ -34,19 +41,58 @@ public class BookingController {
 		return "booking/bookingList";
 	}
 	
-	// 같은 예약 목록 화면에서 삭제 버튼을 통해 삭제
-	// id로 삭제할 지 이름으로 삭제할 지 미정
-	// AJAX 요청
-	// http://localhost:8080/booking/delete-booking
-	//@ResponseBody
-	//@DeleteMapping("/delete-booking")
+//	 같은 예약 목록 화면에서 삭제 버튼을 통해 삭제
+//	 id로 삭제
+//	 AJAX 요청
+//	 http://localhost:8080/booking/delete-booking
+	@ResponseBody
+	@DeleteMapping("/delete-booking")
+	public Map<String, Object> deleteBooking(
+			@RequestParam("id") int id) {
+		
+		// db delete
+		int rowCount = bookingBO.deleteBookingById(id);
+		
+		// 응답값 내리기
+		Map<String, Object> result = new HashMap<>();
+		if (rowCount > 0) {
+			result.put("code", 200);
+			result.put("result", "성공");
+		} else {
+			result.put("code", 500);
+			result.put("error_message", "삭제할 데이터가 없습니다.");
+		}
+		
+		return result;
+	}
 	
 	
 	// 예약하기 화면
 	// http://localhost:8080/booking/make-booking-view
-	//@PostMapping("")
+	@GetMapping("/make-booking-view")
+	public String makeBookingView() {
+		return "booking/makeBooking";
+	}
 	
-	
+	// AJAX 요청 - add 예약
+	@ResponseBody
+	@PostMapping("/make-booking")
+	public Map<String, Object> makeBooking(
+			@RequestParam("name") String name,
+			@RequestParam("date") LocalDate date, // String으로 해도된다.
+			@RequestParam("day") int day,
+			@RequestParam("headcount") int headcount,
+			@RequestParam("phoneNumber") String phoneNumber) {
+		
+		// db insert
+		bookingBO.addBooking(name, date, day, headcount, phoneNumber);
+		
+		// 응답값
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 200);
+		result.put("result", "성공");
+		return result;
+	}
 	
 	
 	// 예약조회 화면
@@ -54,5 +100,21 @@ public class BookingController {
 	@GetMapping("/check-booking-view")
 	public String checkBookingView() {
 		return "booking/checkBooking";
+	}
+	
+	// ajax 요청 = 예약 조회
+	@ResponseBody
+	@PostMapping("/check-booking") // 정보를 숨기기 위해 post
+	public Map<String, Object> checkBooking(
+			@RequestParam("name") String name,
+			@RequestParam("phoneNumber") String phoneNumber) {
+		// db select
+		boolean isDuplication = bookingBO.isDuplicationUrl(name, phoneNumber);
+		
+		// 응답값 JSON
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 200);
+		result.put("is_duplication", isDuplication);
+		return result;
 	}
 }
--- src/main/java/com/quiz/booking/bo/BookingBO.java
+++ src/main/java/com/quiz/booking/bo/BookingBO.java
@@ -1,9 +1,11 @@
 package com.quiz.booking.bo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.quiz.booking.domain.Booking;
 import com.quiz.booking.mapper.BookingMapper;
@@ -13,10 +15,35 @@ public class BookingBO {
 
 	@Autowired
 	private BookingMapper bookingMapper;
+	
 	// input : X
 	// output : List<Booking>
 	public List<Booking> getBookingList() {// db select 단계이기 때문에 method명에 get을 붙인다.
 		
 		return bookingMapper.selectBookingList();
+	}
+	
+	// input: id
+	// output: int(rowCount)
+	public int deleteBookingById(int id) {
+		return bookingMapper.deleteBookingById(id);
+	}
+	
+	
+	// input:
+	// output:
+	
+	public void addBooking(String name, LocalDate date, int day, int headcount, String phoneNumber) {
+		bookingMapper.insertBooking(name, date, day, headcount, phoneNumber);
+
+	}
+	
+	// input: 
+	// output: boolean
+	public boolean isDuplicationUrl(String name, String phoneNumber) {
+		// 중복 있음:List     중복 없음:[]
+		List<Booking> bookingList = bookingMapper.selectBookingListByName(name, phoneNumber);
+		// [] => false, 비어있지 않으면 => true
+		return bookingList.isEmpty() ? false : true;
 	}
 }
