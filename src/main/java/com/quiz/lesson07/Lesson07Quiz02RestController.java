37fb4d7aac3442888d6543999a0dd5755e3da311
DESKTOP-HBIBUUM\user <test@test.com>
2024-07-08 18:53:30 +0900

    0708 학원 수업 quiz

--- src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
+++ src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
@@ -19,14 +19,15 @@ public class Lesson07Quiz02RestController {
 	private RecruitRepository recruitRepository;
 	// 1)
 	@GetMapping("/1")
-	public List<RecruitEntity> select1() {
-		return recruitRepository.findById(8);
+	public RecruitEntity select1() { // 단건 조회이기 때문에 리턴타입이 List가 아님
+		return recruitRepository.findById(8).orElse(null);
 	}
 	
+	// /lesson07/quiz02/2?companyId=1
 	@GetMapping("/2")
 	public List<RecruitEntity> select2(
-			@RequestParam("companyId") int companyId) {
-		return recruitRepository.findByCompanyId(1);
+			@RequestParam("companyId") int companyId ) {
+		return recruitRepository.findByCompanyId(companyId);
 	}
 	
 	@GetMapping("/3")
@@ -35,16 +36,22 @@ public class Lesson07Quiz02RestController {
 	}
 	@GetMapping("/4")
 	public List<RecruitEntity> select4() {
-		return recruitRepository.findByTypeOrSalaryGreaterThan("정규직", 8999);
+		return recruitRepository.findByTypeOrSalaryGreaterThanEqual("정규직", 9000);
 	}
 	@GetMapping("/5")
 	public List<RecruitEntity> select5() {
-		return recruitRepository.findTop3ByOrderBySalaryDesc();
+		return recruitRepository.findTop3ByTypeOrderBySalaryDesc("계약직");
 	}
 	@GetMapping("/6")
 	public List<RecruitEntity> select6() {
 		return recruitRepository.findByRegionAndSalaryBetween("성남시 분당구",7000,8500);
 	}
+	@GetMapping("/7")
+	public List<RecruitEntity> select7() {
+		// 마감일이 2026-04-10 이후이고 연봉이 8100 이상인 정규직 공고를 연봉 내림차순으로 조회하세요.
+		return recruitRepository.findByDeadlineAndSalaryAndType("2026-04-10", 8100, "정규직");
+	}
+	
 	
 	
 }
